7957370391868e68b45f3ebabec06890a653e151
nadimrezaee96@gmail.com <dean.rezaee@camufacturing.com>
2024-04-04 11:59:17 -0400

    changes to naming and core.h

--- Trial/src/Trial/Core.h
+++ Trial/src/Trial/Core.h
@@ -1,3 +1,11 @@
 #pragma once
 
+#ifdef TRIAL_ENABLE_ASSERTS
+	#define TRIAL_ASSERT(x, ...) { if(!(x)) { TRIAL_ERROR("Assertion Failed: {0}", __VA_ARGS__); __debugbreak(); } }
+	#define TRIAL_CORE_ASSERT(x, ...) { if(!(x)) { TRIAL_CORE_ERROR("Assertion Failed: {0}", __VA_ARGS__); __debugbreak(); } }
+#else
+	#define TRIAL_ASSERT(x, ...)
+	#define TRIAL_CORE_ASSERT(x, ...)
+#endif
+
 #define BIT(x) (1 << x)
